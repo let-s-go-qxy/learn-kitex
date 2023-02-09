ff7c78314ac668454eec97440ef6e74262fd595a
wizardAEI <imaei@foxmail.com>
2023-02-09 22:07:30 +0800

    feat: kitex init

---
+++ handler.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	user "learnkitex/kitex_gen/user"
+)
+
+// UserServiceImpl implements the last service interface defined in the IDL.
+type UserServiceImpl struct{}
+
+// UserInfo implements the UserServiceImpl interface.
+func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserRequest) (resp *user.UserResponse, err error) {
+	// TODO: Your code here...
+	msg := "ok"
+	return &user.UserResponse{
+		StatusCode: 0,
+		StatusMsg:  &msg,
+		User: &user.User{
+			Id:            0,
+			Name:          "name",
+			FollowCount:   nil,
+			FollowerCount: nil,
+			IsFollow:      false,
+		},
+	}, nil
+}
